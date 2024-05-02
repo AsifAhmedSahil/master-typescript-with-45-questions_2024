ece659aef6b60bdaaafc4805ce62efe24997a05e
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-02 16:07:15 +0600

    changing guest list solution

---
+++ dist/15_changingGuest.js
@@ -0,0 +1,11 @@
+"use strict";
+// question
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+// • Print a second set of invitation messages, one for each person who is still in your list.
+let guestName = ['sahil', 'asif', 'shishir', 'towsif'];
+const dontcome = guestName[1];
+console.log(`${dontcome} aste parbe na!`);
+guestName.splice(0, 1, 'Asi');
+guestName.forEach((guest) => console.log(`${guest} , would you like to dinner with me?`));
