b69058ec540c25c7e575da12deeaaa79cfa87012
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-02 12:35:17 +0600

    2nd question

---
+++ 2_personalMsg.ts
@@ -0,0 +1,3 @@
+let personName = "sahil"
+
+console.log(`hello, ${personName},would you like to learn some Python today?`)
---
+++ dist/2_personalMsg.js
@@ -0,0 +1,3 @@
+"use strict";
+let personName = "sahil";
+console.log(`hello, ${personName},would you like to learn some Python today?`);
