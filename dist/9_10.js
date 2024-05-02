f8c10703ad4aa1ca2d696895a8be2ed669de721a
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-02 13:07:03 +0600

    9&10

---
+++ dist/9_10.js
@@ -0,0 +1,5 @@
+"use strict";
+// questions: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favNumber = 15;
+let msg = "my fav no. is:";
+console.log(msg, favNumber);
