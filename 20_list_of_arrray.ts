98ce49a84a96d8a80706a97e4f95745ead026b64
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-02 22:54:32 +0600

    solve 20th question

---
+++ 20_list_of_arrray.ts
@@ -0,0 +1,7 @@
+let programmingLanguages : string[] = ['typescript',"javascript",'python'] 
+
+console.log("list of prograaming language is:")
+
+programmingLanguages.forEach((language) =>{
+    console.log(language)
+})
---
+++ dist/20_list_of_arrray.js
@@ -0,0 +1,6 @@
+"use strict";
+let programmingLanguages = ['typescript', "javascript", 'python'];
+console.log("list of prograaming language is:");
+programmingLanguages.forEach((language) => {
+    console.log(language);
+});
