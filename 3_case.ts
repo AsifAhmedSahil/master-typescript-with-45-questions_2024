d3d3398aa2eed529acc0c28dcaa5c145136ad77e
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-02 12:43:19 +0600

    question 3

---
+++ 3_case.ts
@@ -0,0 +1,9 @@
+// question: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personeName = "Asif ahmed sahil"
+
+console.log(personeName.toUpperCase())
+console.log(personeName.toLocaleLowerCase())
+
+// title case with expression
+console.log(personeName.replace(/\b\w/g , (char)=> char.toUpperCase()))
