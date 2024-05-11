5a353eef737f38ad81a223a6b92ad207552642d4
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-11 18:34:51 +0600

    checking username problem solved

---
+++ 21_objects.ts
@@ -0,0 +1,13 @@
+interface Course {
+    courseName: string;
+    location: string;
+    studentNo : number
+}
+
+let Course = {
+    courseName:"Typescript Course",
+    location: "ctg",
+    studentNo: 500
+}
+
+console.log(Course)
